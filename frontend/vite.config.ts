eec1ee42fa8159fe2782d03edea57b73ed2723dd
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-12-09 16:00:21 +0900

    reactアプリを1から作り直す
    `docker compose run --rm frontend npm create vite@latest .`を実行

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // https://shinke1987.net/?p=1123#toc5
-  server: {
-    host: '0.0.0.0',
-    port: 4000,
-  }
 })
