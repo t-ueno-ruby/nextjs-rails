ff8b9ed16567c311d007ad151ce6e9ca83d16102
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-12-09 17:18:57 +0900

    TODO エラーが発生している
    frontend/vite.config.ts
    Cannot find module 'vite' or its corresponding type declarations.ts(2307)

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,13 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   // preset: 'ts-jest',
-  // testEnvironment: "node",
-  // transform: {
-  //   "^.+.tsx?$": ["ts-jest",{}],
-  // },
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: 'jest-environment-jsdom',
+  // 以下にtsconfig.app.jsonの設定を追加する
   transform: {
     '^.+.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }],
   },
-  setupFileAfterEnv: ["./jest.setup.ts"]
+  setupFilesAfterEnv: ['./jest.setup.ts'],
 };
